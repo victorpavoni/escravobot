9572bd853338b7f23aefc436054c19e7cd6910e5
Victor Pavoni <55716916+victorpavoni@users.noreply.github.com>
2020-12-08 18:27:22 -0300

    Update

--- commands/porn.js
+++ commands/porn.js
@@ -1,5 +1,5 @@
 const execute = (bot, msg, args) => {
-  const porns = []
+  const porns = ['https://discord.com/channels/697862611328303114/756209115209924749/785979405624672318']
   const porn = Math.floor(Math.random() * porns.length)
 
   if(msg.content.split(' ')[1] == undefined)
