7ede0dd2750295d61bbeac816ae0d44177f9334d
Victor Pavoni <55716916+victorpavoni@users.noreply.github.com>
2020-12-09 14:30:53 -0300

    Update dado.js

--- commands/dado.js
+++ commands/dado.js
@@ -2,19 +2,13 @@ const execute = async (bot, msg, args) => {
   const dado = Math.floor(Math.random() * 55) + 1
   const ms = await msg.reply("Jogando...")
   console.log(dado);
-  switch (dado) {
-    case 30 < dado:
+    if(30 < dado)
       ms.edit(`Catapimbas! Você tirou ${dado}`)
-      break;
-    case 10 < dado:
+    else if( 10 < dado)
       ms.edit(`Parabéns! Você tirou ${dado}`)
-      break;
-      case 10 > dado:
+    else(10 > dado)
       ms.edit(`KKKKKKKK SE FODEU! Você tirou ${dado}`)
-      break;
   }
-
-}
 
 module.exports = {
   name: "dado",
