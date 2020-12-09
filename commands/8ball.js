1a04b1dd28ff03de19073d7ecbde23bb42c41043
Victor Pavoni <55716916+victorpavoni@users.noreply.github.com>
2020-12-09 17:08:35 -0300

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,15 +5,12 @@ const execute = async (bot, msg, args) => {
     'Melhor você não saber.',
     'Não.'
   ]
+  if(!args[0]) return msg.reply("Você precisa me perguntar algo.")
 
   let resposta = Math.floor(Math.random() * respostas.length)
-
   const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
-
   ms = await msg.channel.send('Pergunta difícil, vamos ver ...')
-  
   await delay(3000)
-
   ms.edit(respostas[resposta])
 }
 
