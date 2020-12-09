e62646c422da96e54d3c80dbdea1cfc723cbbde0
Victor Pavoni <55716916+victorpavoni@users.noreply.github.com>
2020-12-09 15:34:30 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,5 @@
 module.exports = async client => {
-  console.log('Bot iniciado com ' + client.users.size + ' usuários, em ' + client.channels.size + ' canais de ' + client.guilds.size + ' servidores.');
-
-
-  setInterval(function() {
-    client.user.setActivity(`Chutando o saco do pedrinho | !help `, {type: 'WATCHING'});
-  }, 60 * 1000);
+  console.log(`Bot iniciado em ${client.guilds.size} servidores.`);
+  client.user.setGame(`Chutando o saco do pedrinho | !help`);
 
 }
