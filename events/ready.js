6fa75714fb2121b573c1c9a542285f60aadf9f69
Victor Pavoni <55716916+victorpavoni@users.noreply.github.com>
2020-12-09 15:55:55 -0300

    dasdfasfa

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
+  console.log(`Logged in as ${client.user.tag}.`)
   setInterval(() => {
-    console.log(`Logged in as ${client.user.tag}.`)
     client.user.setActivity(`Trabalhando duro | *help | by elitex#0001`, {type: 'PLAYING'})
   }, 60 * 1000)
 }
