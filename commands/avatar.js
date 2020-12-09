c88ca2f77843b35396ea93a73b03209a408efd33
Victor Pavoni <55716916+victorpavoni@users.noreply.github.com>
2020-12-08 23:36:19 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -12,13 +12,10 @@ const execute = (bot, msg, args) => {
   }
   
   let avatar = user.displayAvatarURL({size: 4096, dynamic: true});
-  // 4096 is the new biggest size of the avatar.
-  // Enabling the dynamic, when the user avatar was animated/GIF, it will result as a GIF format.
-  // If it's not animated, it will result as a normal image format.
   
   const embed = new Discord.MessageEmbed()
   .setTitle(`${user.tag} avatar`)
-  .setDescription(`[Avatar URL of **${user.tag}**](${avatar})`)
+  .setDescription(`[URL do AVATAR de **${user.tag}**](${avatar})`)
   .setColor(0x1d1d1d)
   .setImage(avatar)
   
